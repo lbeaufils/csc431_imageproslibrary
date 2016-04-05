cee228cc65a22a8fbff70a4fc99a1901c9aaf7fa
Amanda Baughan <aebaughan@gmail.com>
2016-04-04 22:52:40 -0400

    Update confirm.php
    add CSS styling tabs

--- confirm.php
+++ confirm.php
@@ -4,7 +4,14 @@ session_start();
 
 <!doctype html>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" type="text/css" href="style/style.css" />
+<link rel="stylesheet" type="text/css" href="style/colour_blue.css" />
+<link rel="stylesheet" type="text/css" href="style/image_viewer.css" />
 
+<div id="logo">
+<h1>Image Processing Library</h1>
+</div>
 <html>
 <head><title>confirm</title></head>
 <body>
