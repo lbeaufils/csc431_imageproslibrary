84f49f37fb75842c00e5bc7046618bd5f47ba8a8
Amanda Baughan <aebaughan@gmail.com>
2016-04-04 22:57:51 -0400

    Update logout.php
    Adding CSS Styling tags

--- logout.php
+++ logout.php
@@ -1,3 +1,18 @@
+<!doctype html>
+
+<html>
+<!--Edits start here*-->
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" type="text/css" href="style/style.css" />
+<link rel="stylesheet" type="text/css" href="style/colour_blue.css" />
+<link rel="stylesheet" type="text/css" href="style/image_viewer.css" />
+
+<div id="logo">
+<h1>Image Processing Library</h1>
+</div>
+
+<!--Edits end here-->
+
 <?php
 session_start();
 unset($_SESSION['username']);
@@ -5,3 +20,4 @@ unset($_SESSION['username']);
 die("you have successfully logged out.");
 
 ?>
+</html>
